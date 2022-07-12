ec92e6e6e280be0fc62e575e57ff382cd4657371
JHZZ_mjz <m2562771185@163.com>
2022-07-12 11:05:58 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/jhzz/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhzz.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
